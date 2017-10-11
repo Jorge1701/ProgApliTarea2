4e0ac29a7c8febcdbfaf12078c248a174ffc85c4
Luis <Luis@Laptop>
2017-10-11 04:44:56 -0300

    arreglado el descargar archivo sin dañar

--- src/java/servlets/DownloadFile.java
+++ src/java/servlets/DownloadFile.java
@@ -1,9 +1,12 @@
 package servlets;
 
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,29 +16,49 @@ import javax.servlet.http.HttpServletResponse;
 public class DownloadFile extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+       
             if (request.getSession().getAttribute("usuario") == null || request.getParameter("Ruta") == null) {
                 request.setAttribute("mensaje_error", "Lo siento, no se puede acceder aqui de esa forma");
                 request.getRequestDispatcher("vistas/pagina_error.jsp").forward(request, response);
                 return;
             }
 
+            String ruta = getServletContext().getRealPath("/");
+            String[] parte = ruta.split("Tarea2");
+            String tarea1 = parte[0] + "Tarea1" + File.separator;
+
             String filename = request.getParameter("Ruta");
-            String filepath = "C:\\Users\\brian\\Desktop";
-            response.setContentType("APPLICATION/OCTET-STREAM");
-            response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+            String filepath = tarea1 + "Recursos/Musica/";
+            
+            ServletOutputStream stream = null;
+            BufferedInputStream buf = null;
 
-            FileInputStream fileInputStream = new FileInputStream(filepath + filename);
+            try {
 
-            int i;
-            while ((i = fileInputStream.read()) != -1) {
-                out.write(i);
+                stream = response.getOutputStream();
+                File mp3 = new File(filepath + filename);
+
+                //headers
+                response.setContentType("audio/mpeg");
+                response.addHeader("Content-Disposition","attachment; filename=" + filename);
+                response.setContentLength((int) mp3.length());
+                FileInputStream input = new FileInputStream(mp3);
+                buf = new BufferedInputStream(input);
+                int readBytes = 0;
+                while ((readBytes = buf.read()) != -1) 
+                    stream.write(readBytes);
+                
+
+            } catch (IOException ioe) {
+                throw new ServletException(ioe.getMessage());
+            } finally {
+                if (stream != null) 
+                    stream.close();
+                
+                if (buf != null) 
+                    buf.close();
+                
             }
-
-            fileInputStream.close();
-            out.close();
-        }
     }
 
     @Override
